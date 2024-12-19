6ccac8e3302eaad824deeed456182788f5c0240e
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-12-19 12:37:10 +0530

    feat:add onboarding UI scree

--- src/assets/pages/onboarding/onboard.tsx
+++ src/assets/pages/onboarding/onboard.tsx
@@ -1,33 +1,49 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// swiper_js styles 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules"; 
+import "swiper/css";
+import "swiper/css/navigation";
 
 import '../../css/onboard/onboard.css';
 
-import { Navigation } from 'swiper/modules';
+function SwiperOnboard() {
+    const swiperRef = useRef(null);
 
-const SwiperOnboard: React.FC = () => {
-  return (
-    <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-      <div className="onboard_header">
-        <h2>INTRODUCING FOR THE FIRST TIME</h2>
-      </div>
-    </>
-  );
-};
+    const handleNext = () => {
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slideNext(); 
+        }
+    };
+
+    return (
+        <>
+        <div className="onboard_wrapper">
+            <div>
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper" ref={swiperRef} >
+                    <SwiperSlide>
+                        <img src="images/onboard/1.png" alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="images/onboard/2.png" alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="images/onboard/3.png" alt="" />
+                    </SwiperSlide>
+                </Swiper>
+                <div className="onboard_header">
+                    <h1>INTRODUCING FOR THE FIRST TIME</h1>
+                    <p>The all-new grand championship app that will update your score like never before</p>
+                </div>
+            </div>
+            <div className="crousal_next">
+                <button onClick={handleNext}>Next</button>
+            </div>
+            <div className="skip_button">
+               <a className="skip_button_link" href="#">SKIP →</a>
+            </div>
+        </div>
+        </>
+    );
+}
 
 export default SwiperOnboard;
