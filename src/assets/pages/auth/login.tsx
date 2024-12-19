288a8818feb31f108441a89775f4886fbb3cdf57
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-12-19 11:15:58 +0530

    added page view v1

---
+++ src/assets/pages/auth/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../css/auth/login.css'
+
+const LoginApp: React.FC = () => {
+  return (
+    <>
+        <h1>login world !</h1>
+    </>
+  );
+};
+
+export default LoginApp;
